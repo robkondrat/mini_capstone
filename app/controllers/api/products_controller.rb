99d6de83dcc8b30898694c619a7efae64823f403
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-01-15 11:07:56 -0600

    added products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def all_games_action
+    @product = Product.first
+    render "all_games_view.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get '/all_games_url' => 'products#all_games_action'
+ 
+  end
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
