7da08d8d229957646878d35879ec4cdb7a3604d6
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-01-15 11:37:04 -0600

    created product JSONs

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,39 @@
 class Api::ProductsController < ApplicationController
   def all_games_action
-    @product = Product.first
+    @product = Product.all.each do |game|
+      game
+    end
     render "all_games_view.json.jb"
   end
+
+  def cod_action
+    @product = Product.first
+    render "cod_view.json.jb"
+  end
+
+  def forza_action
+    @product = Product.second
+    render "forza_view.json.jb"
+  end
+
+  def smash_action
+    @product = Product.third
+    render "smash_view.json.jb"
+  end
+
+  def doom_action
+    @product = Product.fourth
+    render "doom_view.json.jb"
+  end
+
+  def party_action
+    @product = Product.fifth
+    render "party_view.json.jb"
+  end
+
+  def gta_action
+    @product = Product.last
+    render "gta_view.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_games_url' => 'products#all_games_action'
+    get '/cod_url' => 'products#cod_action'
+    get '/forza_url' => 'products#forza_action'
+    get '/doom_url' => 'products#doom_action'
+    get '/smash_url' => 'products#smash_action'
+    get '/party_url' => 'products#party_action'
+    get '/gta_url' => 'products#gta_action'
  
   end
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+ 
 end
