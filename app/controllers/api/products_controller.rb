33751386e27d556b6574879ddc6e8cb82090d64d
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-01-17 11:29:21 -0600

    added destroy

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,9 +4,38 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @product = Product.new(
+                            name: params[:name],
+                            price: params[:price],
+                            image_url: params[:image_url],
+                            description: params[:description]
+                            )
+    @product.save
+    render "show.json.jb"
+  end
+
   def show
     @product = Product.find(params[:id])
     render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.delete
+    render json: {message: "Product ID #{product.id} successfully deleted."}
   end
 
 end
